9614a66a87bf52fe5413830fdd0723f3e4cfe014
Luke Feng <123667224+LukeF2@users.noreply.github.com>
2024-05-05 15:24:48 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+import os
+TRAINING_FILES_PATH = '/Users/lukefeng/Documents/EmotionRecognition/dataset_features'
+TESS_ORIGINAL_FOLDER_PATH = '/Users/lukefeng/Documents/EmotionRecognition/TESS Toronto emotional speech set data'
+
+
+print(os.path.exists('/Users/lukefeng/Documents/EmotionRecognition'))
+print(os.path.exists(
+    '/Users/lukefeng/Documents/EmotionRecognition/TESS Toronto emotional speech set data'))
+print(os.path.exists('/Users/lukefeng/Documents/EmotionRecognition/dataset_features'))
